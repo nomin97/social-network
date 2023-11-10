29ac959524dec34fda1d3cc59247b044a388a608
Nomin Myagmarsuren <nominmyagmarsuren@gmail.com>
2023-11-09 22:41:22 -0700

    install mongodb, require, connections

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+// Run npm install mongodb and require mongodb and MongoClient class
+const { MongoClient } = require('mongodb');
+
+const app = express();
+const port = 3001;
+
+// Connection string to local instance of MongoDB
+const connectionStringURI = `mongodb://127.0.0.1:27017`;
+
+// Initialize a new instance of MongoClient
+const client = new MongoClient(connectionStringURI);
+
+// Declare a variable to hold the connection
+let db;
+
+// Create variable to hold our database name
+const dbName = '   ';
+
+// Use connect method to connect to the mongo server
+client.connect()
+  .then(() => {
+    console.log('Connected successfully to MongoDB');
+    // Use client.db() constructor to add new db instance
+    db = client.db(dbName);
+
+    // start up express server
+    app.listen(port, () => {
+      console.log(`Example app listening at http://localhost:${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error('Mongo connection error: ', err.message);
+  });
